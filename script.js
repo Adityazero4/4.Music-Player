22faffc750113135f4067ee8f42b293d92d3bda1
Aditya Jain <20io1004@mitsgwl.ac.in>
2022-12-22 01:42:57 +0530

    new file:   img/Aditya_4.jpg 	new file:   img/Aditya_5.jpg 	new file:   img/Aditya_6.jpg 	deleted:    img/jacinto-1.jpg 	deleted:    img/jacinto-2.jpg 	deleted:    img/jacinto-3.jpg 	modified:   index.html 	new file:   music/Aditya_4.mp3 	new file:   music/Aditya_5.mp3 	new file:   music/Aditya_6.mp3 	deleted:    music/jacinto-1.mp3 	deleted:    music/jacinto-2.mp3 	deleted:    music/jacinto-3.mp3 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -2,30 +2,34 @@ const img = document.querySelector("img");
 const title = document.getElementById("title");
 const artist = document.getElementById("artist");
 const music = document.querySelector("audio");
+const progressContainer = document.getElementById("progress-container");
+const progress = document.getElementById("progress");
+const currentTimeEl = document.getElementById("current-time");
+const durationEl = document.getElementById("duration");
 const prevBtn = document.getElementById("prev");
 const playBtn = document.getElementById("play");
 const nextBtn = document.getElementById("next");
 
 const songs = [
   {
-    name: "jacinto-1",
-    displayName: "Electric Chill Machine",
-    artist: "Jacinto Design",
-  },
-  {
-    name: "jacinto-2",
-    displayName: "Seven Nation Army (Remix)",
-    artist: "Jacinto Design",
-  },
-  {
-    name: "jacinto-3",
-    displayName: "Good Night, Disco Queen",
-    artist: "Jacinto Design",
+    name: "Aditya_4",
+    displayName: "Maan Meri Jaan",
+    artist: "King",
   },
   {
     name: "metric-1",
     displayName: "Front Row (Remix)",
     artist: "Jacinto Design",
+  },
+  {
+    name: "Aditya_5",
+    displayName: "The Search",
+    artist: "NF",
+  },
+  {
+    name: "Aditya_6",
+    displayName: "Hall of Fame",
+    artist: "Script",
   },
 ];
 
@@ -61,7 +65,6 @@ function nextSong() {
   if (songIndex > songs.length - 1) {
     songIndex = 0;
   }
-  console.log(songIndex);
   loadSong(songs[songIndex]);
   playSong();
 }
@@ -70,10 +73,46 @@ function prevSong() {
   if (songIndex < 0) {
     songIndex = songs.length - 1;
   }
-  console.log(songIndex);
   loadSong(songs[songIndex]);
   playSong();
+}
+loadSong(songs[songIndex]);
+
+function updateProgressBar(e) {
+  if (isplaying) {
+    const { duration, currentTime } = e.srcElement;
+
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+
+    const durationMinutes = Math.floor(duration / 60);
+    let durationSeconds = Math.floor(duration % 60);
+    if (durationSeconds < 10) {
+      durationSeconds = `0${durationSeconds}`;
+    }
+
+    if (durationSeconds) {
+      durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
+    }
+
+    const currentMinutes = Math.floor(currentTime / 60);
+    let currentSeconds = Math.floor(currentTime % 60);
+    if (currentSeconds < 10) {
+      currentSeconds = `0${currentSeconds}`;
+    }
+    currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
+  }
+}
+
+function setProgressBar(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const { duration } = music;
+  music.currentTime = (clickX / width) * duration;
 }
 
 prevBtn.addEventListener("click", prevSong);
 nextBtn.addEventListener("click", nextSong);
+music.addEventListener("timeupdate", updateProgressBar);
+progressContainer.addEventListener("click", setProgressBar);
+music.addEventListener("ended", nextSong);
